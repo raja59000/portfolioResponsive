f826a615da77e076410e9b5f3bd13e1e005de662
raja59000 <79079642+raja59000@users.noreply.github.com>
2022-07-04 14:23:53 +0200

    ajout de l'apparition du back to top on scrollii

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top:'+(e.pageY - 20)+"px; left:"+(e.pageX - 20)+"px;")
+})
+
+document.addEventListener('click', ()=>{
+    cursor.classList.add('expand');
+
+    setTimeout(()=>{
+        cursor.classList.remove("expand");
+    }, 500);
+})
+
+function changebg(){
+    document.getElementById('projets').style.background = "black";
+    document.getElementById('projets2').style.background = "black";
+    document.getElementById('title-projet').style.color = "#7451EB";
+}
+
+function changebginit(){
+    document.getElementById('projets').style.background = "#3e3e3e";
+    document.getElementById('projets2').style.background = "#3e3e3e";
+    document.getElementById('title-projet').style.color = "white";
+}
+
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 600) {
+      $('#backtop').show();
+    } else {
+      $('#backtop').hide();
+    }
+  });
+ 
